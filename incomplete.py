c315a5fa5a576cae86e2662c2e6ca82394ca7342
Michael Kristofik <kristo605@gmail.com>
2024-01-26 22:01:05 -0600

    Output html for easier copy and pasting to TroopTrack

--- incomplete.py
+++ incomplete.py
@@ -1,14 +1,22 @@
+"""Who has started working on badges but not finished them yet?"""
+
 import ahg
+from collections import defaultdict
+import webbrowser
 
 
 if __name__ == '__main__':
     roster = ahg.Roster()
+    incomplete = defaultdict(list)
     for girl in roster.names():
-        allUndone = list(roster.incomplete_badges(girl))
-        if not allUndone:
-            continue
-        # TODO: produce html output so the girls' names are bolded
-        print(girl)
-        for badge in allUndone:
-            print(badge)
-        print()
+        for badge in roster.incomplete_badges(girl):
+            incomplete[girl].append(badge)
+
+    with open('incomplete.html', 'w') as f:
+        for girl, badges in incomplete.items():
+            print('<p><b>' + girl + '</b><br />', file=f)
+            for badge in badges:
+                print(badge + '<br />', file=f)
+            print('</p>', file=f)
+
+    webbrowser.open('incomplete.html', new=2)
